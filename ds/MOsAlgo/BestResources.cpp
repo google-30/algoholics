ea6ca9cba7ec28375b07e3c02ea0c4f9c4a03c48
shubham jain <s1996jain@gmail.com>
2018-01-03 23:23:16 +0530

    Create BestResources.cpp

---
+++ ds/MOsAlgo/BestResources.cpp
@@ -0,0 +1,15 @@
+https://www.geeksforgeeks.org/mos-algorithm-query-square-root-decomposition-set-1-introduction/
+
+https://blog.anudeep2011.com/mos-algorithm/
+
+https://www.hackerearth.com/practice/notes/mos-algorithm/
+
+
+Problems
+
+1: http://www.spoj.com/problems/DQUERY
+2: http://www.spoj.com/problems/ZQUERY/
+3: http://codeforces.com/contest/86/problem/D  (POWERFULL ARRAYS)
+4: https://www.codechef.com/problems/IITI15 (Sherlock and Inversions)
+
+
