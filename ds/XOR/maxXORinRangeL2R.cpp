f0f85b709e295f9d317d5a28acfd35df26e91484
shubham jain <s1996jain@gmail.com>
2018-02-28 17:28:32 +0000

    Create maxXORinRangeL2R.cpp

---
+++ ds/XOR/maxXORinRangeL2R.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+#define ll long long 
+
+ll maxXOR(ll l,ll r)
+{
+    ll p,c=0,ans;
+    p = l ^ r;
+    while(p)
+    {
+        p/=2;
+        c++;
+    }
+    
+    ans = (1<<c);
+    ans--;
+    return ans;
+}
+int main() {
+	
+	ll l,r;
+	cin>>l>>r;
+	cout<<maxXOR(l,r)<<endl;
+	return 0;
+}
